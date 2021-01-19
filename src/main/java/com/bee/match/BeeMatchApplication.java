2a111f6564329d527c1c4f648370b93090fc771d
will <suimin@cointiger.com>
2021-01-19 18:43:21 +0800

    fix

--- src/main/java/com/bee/match/BeeMatchApplication.java
+++ src/main/java/com/bee/match/BeeMatchApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BeeMatchApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(BeeMatchApplication.class, args);
     }
